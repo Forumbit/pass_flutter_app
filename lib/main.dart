5a4fcf4500edfd9ac616e0fba6a3dcaa36e9ff89
Amir Kharisov <amirkharisov545@Gmail.com>
2023-03-30 21:21:12 +0300

    fourth commit

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Git PULL 1'),
+      home: const MyHomePage(title: 'Git PULL 2'),
     );
   }
 }
